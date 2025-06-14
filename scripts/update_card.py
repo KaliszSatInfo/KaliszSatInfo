10f9d0bba137f8fec46e941b27d1717a143fd034
KaliszSatInfo <148968884+KaliszSatInfo@users.noreply.github.com>
2025-06-14 13:19:24 +0200

    Update update_card.py

--- scripts/update_card.py
+++ scripts/update_card.py
@@ -119,7 +119,7 @@ def apply_penalty_formula(repo_language_data):
     normalized = {}
     for lang, score in adjusted_scores.items():
         percent = round(score / total * 100, 2)
-        if percent > 0.5 or lang in {"Python", "TypeScript" "Java"}:
+        if percent > 0.5 or lang in {"Python", "TypeScript", "Java"}:
             normalized[lang] = percent
     return dict(sorted(normalized.items(), key=lambda x: x[1], reverse=True))
 
